bc9a2131b418f0e4414da22932d40b57608f296d
gpires <gpires@spotify.com>
2020-09-09 16:48:54 +0200

    Improve Artifactory HEAD request

--- zip-and-push-to-artifactory.sh
+++ zip-and-push-to-artifactory.sh
@@ -11,7 +11,7 @@ git archive --format=zip HEAD -o "${ZIP_FILE}"
 
 # Check if SHA is already present in artifactory for whatever reason
 echo "Checking if ${ZIP_FILE} is already in Artifactory"
-RESPONSE_CODE=$(curl --head --write-out '%{http_code}' --silent --output /dev/null "https://artifactory.spotify.net/artifactory/cerbero-tests/from-gabriel/${ZIP_FILE}")
+RESPONSE_CODE=$(curl --fail -m300 --head --write-out '%{http_code}' --silent --output /dev/null "https://artifactory.spotify.net/artifactory/cerbero-tests/from-gabriel/${ZIP_FILE}")
 
 if [ "$RESPONSE_CODE" = 200 ]; then
   echo "Skipping since artifact is already published"
