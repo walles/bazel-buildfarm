5ba2d86f91983f1625ffc54f4415457d4d4577b8
gpires <gpires@spotify.com>
2020-09-10 16:54:30 +0200

    Reduce verbosity

--- build.sh
+++ build.sh
@@ -7,14 +7,14 @@ apt-get install -y curl unzip g++
 curl -sS -m300 -Lo bazelisk https://artifactory.spotify.net/artifactory/client-infrastructure/buildtools/bazelisk/1.2.1_2/bazelisk-linux-amd64
 chmod 755 bazelisk
 ./bazelisk clean
-./bazelisk build //src/main/java/build/buildfarm:all --verbose_failures
+./bazelisk build //src/main/java/build/buildfarm:all
 
 echo "running from folder:"
 pwd
 ls -l
 set +ex
 # Exclude common-tests target since it fails on Tingle for some reason
-./bazelisk test -- //src/test/java/build/buildfarm:all -//src/test/java/build/buildfarm:common-tests --verbose_failures
+./bazelisk test -- //src/test/java/build/buildfarm:all -//src/test/java/build/buildfarm:common-tests
 status=$?
 find ../.cache -iname test.log -exec cat {} \;
 exit $status
