e6c700e2eea41ff134fc1d4d5d67521268b047a2
gpires <gpires@spotify.com>
2020-09-15 10:51:59 +0200

    [publishing] Fix script breaking on 404 HEAD responses

--- zip-and-push-to-artifactory.sh
+++ zip-and-push-to-artifactory.sh
@@ -13,11 +13,11 @@ git archive --format=zip HEAD -o "${ZIP_FILE}"
 
 # Check if SHA is already present in artifactory for whatever reason
 echo "Checking if ${ZIP_FILE} is already in Artifactory"
-RESPONSE_CODE=$(curl --fail -m300 --head --write-out '%{http_code}' --silent --output /dev/null "https://artifactory.spotify.net/artifactory/cerbero-tests/from-gabriel/${ZIP_FILE}")
+RESPONSE_CODE=$(curl -m300 --head --write-out '%{http_code}' --silent --output /dev/null "https://artifactory.spotify.net/artifactory/cerbero-tests/from-gabriel/${ZIP_FILE}")
 
-if [ "$RESPONSE_CODE" = 200 ]; then
-  echo "Skipping since artifact is already published"
-else
+if [ "$RESPONSE_CODE" = 404 ]; then
   echo "Publishing ${ZIP_FILE} to Artifactory"
   curl --fail -m300 -X PUT "https://artifactory.spotify.net/artifactory/cerbero-tests/from-gabriel/${ZIP_FILE}" -T "${ZIP_FILE}"
+else
+  echo "Skipping since artifact is already published"
 fi
