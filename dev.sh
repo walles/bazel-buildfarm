65bcfeec8733664d5acf5dbd3376dfc09545b92d
Thomas Nagy <thna@spotify.com>
2020-10-23 10:47:47 +0200

    Added development instructions and a script to help us profile remotely

---
+++ dev.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+set -ex
+
+bazelisk build //src/main/java/build/buildfarm:buildfarm-shard-worker_deploy.jar
+
+scp bazel-bin/src/main/java/build/buildfarm/buildfarm-shard-worker_deploy.jar gew1-preprodbuildfarmworker-d-076x.gew1.spotify.net:~/
+
+# on the remote:
+# disco role preprodbuildfarmworker to pick one instance
+# systemctl stop helios-agent.service
+# sudo java -XX:StartFlightRecording=delay=5s,duration=120s,settings=profile,filename=/tmp/$(date +%Y_%m_%d-%H_%M_%S).jfr -jar buildfarm-shard-worker_deploy.jar /etc/spotify/worker.config
+# 
+
+ssh gew1-preprodbuildfarmworker-d-076x.gew1.spotify.net sudo java -XX:StartFlightRecording=delay=5s,duration=120s,settings=profile,filename=/tmp/$(date +%Y_%m_%d-%H_%M_%S).jfr -jar buildfarm-shard-worker_deploy.jar /etc/spotify/worker.config
+
